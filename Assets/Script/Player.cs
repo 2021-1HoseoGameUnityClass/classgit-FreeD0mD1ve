3fd4e16da0d0656d947c241715f0de6e9b17d285
FreeD0mD1ve <youbin11111@naver.com>
2021-05-21 01:18:34 +0900

    Revert "12주차"
    This reverts commit 6b9bdea11bf02a6074fb38af246d2781bfc0d6e6.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,36 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{  
-    [SerializeField] // 프라이베이트여도 유니티에서 설정 가능
+{
+    [SerializeField]
     private float moveSpeed = 3f;
 
-    [SerializeField]
-    private float jumpForce = 300f;
-    public bool isJump = false;
-
-    [SerializeField]
-    private GameObject bulletObj = null;
-
-    [SerializeField]
-    private GameObject InstantiateObj = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
-    {
-        PlayerMove();
-
-        if(Input.GetButtonDown("Jump"))
-        {
-            PlayerJump();
-        }
-
-        if(Input.GetButtonDown("Fire1"))
-        {
-            Fire();
-        }
-    }
-    private void PlayerMove()
     {
         float h = Input.GetAxis("Horizontal");
         float playerSpeed = h * moveSpeed * Time.deltaTime;
@@ -41,12 +23,12 @@ public class Player : MonoBehaviour
 
         transform.Translate(vector3);
 
-        if (h < 0)
+        if(h < 0)
         {
             GetComponent<Animator>().SetBool("Walk", true);
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        else if (h == 0)
+        else if ( h == 0 )
         {
             GetComponent<Animator>().SetBool("Walk", false);
         }
@@ -56,36 +38,5 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
         }
 
-    }
-
-    private void PlayerJump()
-    {
-        //점프상태 아닐때만 
-        if (isJump == false)
-        {
-            GetComponent<Animator>().SetBool("Walk", false);
-            GetComponent<Animator>().SetBool("Jump", true);
-
-            Vector2 vector2 = new Vector2(0, jumpForce);
-            GetComponent<Rigidbody2D>().AddForce(vector2);
-            isJump = true;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.tag == "Platform")
-        {
-            GetComponent<Animator>().SetBool("Jump", false);
-            isJump = false;
-        }
-    }
-
-    public void Fire()
-    {
-        float direction = transform.localScale.x;
-        Quaternion quaternion = new Quaternion(0, 0, 0, 0);
-
-       Instantiate(bulletObj, InstantiateObj.transform.position, quaternion).GetComponent<Bullet>().IntantiateBullet(direction);
     }
 }
