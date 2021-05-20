67e675e4edb97403dcf2dc4c09a69e8eba54af88
FreeD0mD1ve <youbin11111@naver.com>
2021-05-21 01:22:17 +0900

    12주차

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,18 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-    [SerializeField]
+{  
+    [SerializeField] // 프라이베이트여도 유니티에서 설정 가능
     private float moveSpeed = 3f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float jumpForce = 300f;
+    public bool isJump = false;
+
+    [SerializeField]
+    private GameObject bulletObj = null;
+
+    [SerializeField]
+    private GameObject InstantiateObj = null;
 
     // Update is called once per frame
     void Update()
+    {
+        PlayerMove();
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            PlayerJump();
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+    private void PlayerMove()
     {
         float h = Input.GetAxis("Horizontal");
         float playerSpeed = h * moveSpeed * Time.deltaTime;
@@ -23,12 +41,12 @@ public class Player : MonoBehaviour
 
         transform.Translate(vector3);
 
-        if(h < 0)
+        if (h < 0)
         {
             GetComponent<Animator>().SetBool("Walk", true);
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        else if ( h == 0 )
+        else if (h == 0)
         {
             GetComponent<Animator>().SetBool("Walk", false);
         }
@@ -38,5 +56,36 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(1, 1, 1);
         }
 
+    }
+
+    private void PlayerJump()
+    {
+        //점프상태 아닐때만 
+        if (isJump == false)
+        {
+            GetComponent<Animator>().SetBool("Walk", false);
+            GetComponent<Animator>().SetBool("Jump", true);
+
+            Vector2 vector2 = new Vector2(0, jumpForce);
+            GetComponent<Rigidbody2D>().AddForce(vector2);
+            isJump = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Platform")
+        {
+            GetComponent<Animator>().SetBool("Jump", false);
+            isJump = false;
+        }
+    }
+
+    public void Fire()
+    {
+        float direction = transform.localScale.x;
+        Quaternion quaternion = new Quaternion(0, 0, 0, 0);
+
+       Instantiate(bulletObj, InstantiateObj.transform.position, quaternion).GetComponent<Bullet>().IntantiateBullet(direction);
     }
 }
